32ff6fe92264d347dd4113a131856acc30cf465e
PascalButterbach <pascal@butterbach.org>
2022-02-08 23:22:39 +0100

    -

--- src/main/java/org/butterbach/manageddatabasespoc/CmdRunner.java
+++ src/main/java/org/butterbach/manageddatabasespoc/CmdRunner.java
@@ -6,6 +6,8 @@ import org.butterbach.manageddatabasespoc.service.CustomerService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.UUID;
 
 @Component
@@ -19,14 +21,39 @@ public class CmdRunner implements CommandLineRunner {
 
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
 
-        UUID uuid = UUID.fromString("11111111-1111-1111-1111-111111111111");
-        //UUID uuid = UUID.fromString("22222222-2222-2222-2222-222222222222");
+        ArrayList<Thread> tenantOneThreads = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            tenantOneThreads.add(
+                    new Thread(() -> {
+                        for (int j = 0; j < 1000; j++) {
 
+                            UUID uuid1 = UUID.fromString("11111111-1111-1111-1111-111111111111");
+                            TenantDatabaseContextHolder.set(uuid1);
+                            customerService.create(new Customer(null, LocalDateTime.now().toString()));
+                            TenantDatabaseContextHolder.clear();
+                        }
+                    })
+            );
+        }
 
-        TenantDatabaseContextHolder.set(uuid);
-        customerService.create(new Customer(null, "new one"));
-        TenantDatabaseContextHolder.clear();
+        ArrayList<Thread> tenantTwoThreads = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            tenantTwoThreads.add(
+                    new Thread(() -> {
+                        for (int j = 0; j < 1000; j++) {
+
+                            UUID uuid1 = UUID.fromString("22222222-2222-2222-2222-222222222222");
+                            TenantDatabaseContextHolder.set(uuid1);
+                            customerService.create(new Customer(null, LocalDateTime.now().toString()));
+                            TenantDatabaseContextHolder.clear();
+                        }
+                    })
+            );
+        }
+
+        tenantOneThreads.forEach(Thread::start);
+        tenantTwoThreads.forEach(Thread::start);
     }
 }
